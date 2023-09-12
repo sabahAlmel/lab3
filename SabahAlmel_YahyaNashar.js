f2cfaeda96ca7b0db8fe98d0b2791aec9035dce6
Sabah <sabahalmel7@gmail.com>
2023-09-12 16:02:50 +0300

    fixing the second ex

--- SabahAlmel_YahyaNashar.js
+++ SabahAlmel_YahyaNashar.js
@@ -3,18 +3,19 @@
 // then we assign each of max and min to the first element in this array
 // then a loop to see if we passed an element greater than the max ..then the max it will be this nb
 // and if this value is less than the min so we replace the min by this value
-// var array,min,max;
-// var array=prompt("enter an array like (1,6,2,3)");
-// array=array.split(",")
-// min=max=array[0]
-// for(let i=0;i<array.length;i++){
-//     switch(true){
-//         case (array[i]>max):max=array[i];break;
-//         case (array[i]<min):min=array[i];break
-//     }
-// }
-// console.log("minimum is :"+min)
-// console.log("max is:"+max)
+var array,min,max;
+var array=prompt("enter an array like (1,6,2,3)");
+array=array.split(",")
+console.log(array)
+min=max=array[0]
+for(let i=0;i<array.length;i++){
+    switch(true){
+        case (parseInt(array[i])>max):max=array[i];break;
+        case (parseInt(array[i])<min):min=array[i];break
+    }
+}
+console.log("minimum is :"+min)
+console.log("max is:"+max)
 
 
 // ex3
@@ -24,10 +25,13 @@
 // j=0;j<=0 yes so it goes and print * and finished the second loop then go to first one and go to new line
 // j=0;j<=1 yeas so print ** then go to first loop and go to new line etc...
 
-let nb=prompt("enter a number")
-for(let i=0;i<nb;i++){
-    for(let j=0;j<=i;j++){
-        console.log("*")
-    }
-    console.log("\n");
-}
+// let nb=prompt("enter a number")
+// for(let i=0;i<nb;i++){
+//     for(let j=0;j<=i;j++){
+//         console.log("*")
+//     }
+//     console.log("\n");
+// }
+
+// ex4
+

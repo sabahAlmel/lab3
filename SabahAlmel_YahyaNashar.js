ee52bef6675a2960b993cb4b0b18f75f78018908
yahya nashar <cenations.people@live.com>
2023-09-12 15:39:38 +0300

    exercise 5

--- SabahAlmel_YahyaNashar.js
+++ SabahAlmel_YahyaNashar.js
@@ -1,17 +1,26 @@
 // Exercise 1  :
 
-var keys = parseInt(prompt("enter the number of keys"));
- switch (keys){
-    case 0 :
-        console.log("We sank together");
-        break;
-    case 30000 :
-        alert("We made it")
-        break;
-    default :
-        console.log("We need to swim a little bit more!");
-        break ; 
+// var keys = parseInt(prompt("enter the number of keys"));
+//  switch (keys){
+//     case 0 :
+//         console.log("We sank together");
+//         break;
+//     case 30000 :
+//         alert("We made it")
+//         break;
+//     default :
+//         console.log("We need to swim a little bit more!");
+//         break ; 
 
- }
+//  }
 
 // we are basically switching the value of keys by 0 or 30000 , if it's any other number we have the default value set...
+
+
+// Exercise 5:
+
+function GiveMeNumber(num){
+    var num = parseInt(prompt("enter a number"));
+    return alert("num*num");
+}
+GiveMeNumber();

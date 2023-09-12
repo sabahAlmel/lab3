f579ab33506bbe783e41789cc857d42218c133d2
yahya nashar <cenations.people@live.com>
2023-09-12 15:33:05 +0300

    first exercise

---
+++ SabahAlmel_YahyaNashar.js
@@ -0,0 +1,17 @@
+// Exercise 1  :
+
+var keys = parseInt(prompt("enter the number of keys"));
+ switch (keys){
+    case 0 :
+        console.log("We sank together");
+        break;
+    case 30000 :
+        alert("We made it")
+        break;
+    default :
+        console.log("We need to swim a little bit more!");
+        break ; 
+
+ }
+
+// we are basically switching the value of keys by 0 or 30000 , if it's any other number we have the default value set...
